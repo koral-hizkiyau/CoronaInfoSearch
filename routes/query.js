238feeeed03f0af6a5098f0fa81beaac3ba9018f
koral hizkiyau <61585370+koral-hizkiyau@users.noreply.github.com>
2022-06-22 22:14:40 +0300

    u

---
+++ routes/query.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+const axios = require("axios");
+/* GET home page. */
+router.get('/', (req, res) => {
+
+    res.render('corona', { title: 'Country Search' });
+});
+
+
+router.get('/:case/', (req, res) => {
+  let url = `https://coronavirus-19-api.herokuapp.com/countries?fbclid=IwAR12apyTstUwicIA8frJOFJfyHbwXrpU9TMMhWuYy1Rjj78Hzqb60f53YAs`;
+  axios.get(url)
+  .then(resp => {
+    let searchQ = req.query.q;
+    let myId =  resp.data;
+    console.log(req.query);
+    
+    let myAr = myId.filter(item =>{
+        return ((searchQ < item.casesPerOneMillion))
+    });
+    res.render('country', {_ar: myAr});
+  })
+});
+
+module.exports = router;
